1e9d9e60abdfa79642d5b81dbe7168ec65aa946b
wanglongzhen <1962955586@qq.com>
2023-12-05 09:15:41 +0800

    feat: 移动端登录界面

--- vite.config.ts
+++ vite.config.ts
@@ -49,8 +49,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/w': {
-        // target: 'http://59.110.7.8:3000',
-        target: 'http://localhost:3000',
+        target: 'http://59.110.7.8:3000',
+        // target: 'http://localhost:3000',
         rewrite: (path) => path.replace(/^\/w/, '')
       }
     }
